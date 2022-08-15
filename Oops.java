0553b5833bcc47dd662b185540b9dab9a43bf288
pandiselviN <109744350+pandiselviN@users.noreply.github.com>
2022-08-15 15:30:19 +0530

    Add files via upload

---
+++ Oops.java
@@ -0,0 +1,61 @@
+1.   oops stands for 
+      object oriented programming language. 
+2.polymorphism: 
+	The ability of a message to be displayed in more than one form.
+	It helps the programmer to reuse the code.abstract
+Types:
+	* compile time polymorphism:
+		If same argument is passed in two methods ,compile time error will be araise.
+	* method overloading:
+		The same class ,same method with different argument.It is static binding. 
+	* method overriding:
+		.The different class,same method with different argument.abstract
+		.It is dynamic binding and runtime compliationn
+	
+3.Interface:
+	Interface is a design which has unimplement methods ends with semicolon and  we cannot creat a object
+	only two access modifier can use in interface ,they are
+	  * public
+	  * default
+Ex in selenium:
+	chromeDriver extends chromiumDriver
+	chromiumDriver extends remoteWebDriver
+Ex in Java:
+	arrayList extend abstractList
+	abstractList extends abstractCollection
+4.Abstract class:
+	* Essential to providing an absraction to the code to make it reusable and extendable
+	* It has partially impement and unimplement methods
+	* Absract is a keyword used for future use 
+Ex: abstract class Car{...}
+5. * Multiple inheritance:
+	 Is when a class inherits from many base classes .
+	 Multiple inheritance is possible in interface.
+	* Multilevel inheritance:
+		Is when a class inherits from a derived class making that derived class a base class for a new class
+		Multilevel inheritance is possible in class level.
+6.
+No ,multiple inheritance	is not possible in java .
+7. Inhertiance keyword:
+     extends is the keyword for using inheritance
+8.keyword to connect interface and class:
+	implements is the keyword to connect interface and class.
+9.class:
+	* A class describes the attributes and behaviors of an object.
+	* A class may contain abstract methods ,concrete methods.
+	members of class can be public ,private,protected or default.
+Interface:
+	* An interface contains behaviors that class implements.
+	* An interface contains only abstact methods.
+	* All the members of interface are public by default.
+10 Class:
+	* class can declared as final.
+	* class method can have body.
+	Abstract class :
+		* Abstract class can have static fields and static methods ,like other classes.
+		* Abstract method cannot have body
+		
+		
+
+	
+	  
